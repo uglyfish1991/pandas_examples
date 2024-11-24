972aa60bd45d58e6da331a1c138f7c023e09a5e7
Katy Long <katy.long@wearecodenation.com>
2024-11-24 18:28:43 +0000

    using a frame notes finished

--- d_exploration.py
+++ d_exploration.py
@@ -1,15 +1,32 @@
+
+#* --------------------- Data Exploration in Pandas ----------------------------------
+
+#? Data exploration is the intitial step for you, as the analyst, to learn about the data you are working with.
+#? It's not necessarily going to answer questions for the problem statement, but it will help you be more informed about your data.
+
+#* --------------------- Reading the Data In ----------------------------------
 import pandas as pd
 
 df = pd.read_csv("sup_results.csv")
 
-print(df.query('home_team=="England"').groupby('tournament')['home_team'].count())
+#* --------------------- Exploration Methods ----------------------------------
+
+#? df.info() prints information about your frame. It gives information like the number of columns and rows, how many cells have missing values, the column names, and the data type of the columns.
+#? It gives no analytical information, but is useful to introduce you to the frame.
+df.info()
+
+#? df.describe() produces descriptive statistics for any numerical column. These figures include:
+#? The five-figure summary (min, 25%, median, 75%. max)
+#? The mean (adding up all the values in the column and dividing by the number of values - gives a value which can represent the data, an average) 
+#? The standard deviation (the average distance a value is from the mean. It shows how much the data varies)
+#? The count (how many values are in the column)
 
 print(df.describe())
+#? These descriptive statistics can tell us lots about our data wuthout us having to trawl through row by row. 
+#? We discovered that 75% of our data is made up of the numbers 0,1,and 2. 
+#? The last 25% of our data is made up of the numbers 3-31! 
 
+#? We need to see how much of an impact that last 25% of our data is having on our stats. A large dataset is not as impacted by outliers, they don't hold as much weight.
+#? I can use the value_count() method to see how the values make up the data. Because I've been referencing percentiles throughout, I can normalise the data to give the realtive frequencies, and * by 100 turns this into the percentage.
 print(df["home_score"].value_counts(normalize=True)*100)
 
-mask = df["home_score"]<4
-
-masked_df = df[mask]
-
-print(masked_df["home_score"].mean())
--- e_using_a_frame.py
+++ e_using_a_frame.py
@@ -24,21 +24,44 @@ print(df["playlist_genre"].mode())
 #? If we JUST want a single result, and not the box the mode comes in we can use index position:
 print(df["playlist_genre"].mode()[0])
 
-# print(df["duration_ms"].median())
-# print(df["duration_ms"].mean())
+#? The mean and median of a column can be found using the below methods.
+#? Notice how consistent the syntax is - dot notation is very useful!
 
-# max_ms = df["duration_ms"].max()
-# min_ms = df["duration_ms"].min()
-# print(max_ms - min_ms)
+print(df["duration_ms"].median())
+print(df["duration_ms"].mean())
 
-# print(df["duration_ms"].sum())
+#? There is no specific "range" function in Pandas - instead, we can find the range using .min() and .max()
+max_ms = df["duration_ms"].max()
+min_ms = df["duration_ms"].min()
+print(max_ms - min_ms)
 
-# print(df.sort_values(by=["duration_ms"]))
+#? We can sum the total of a column using the .sum() method.
+print(df["duration_ms"].sum())
 
-# print(df.groupby('playlist_genre')["duration_ms"].max())
+#*  -------------  Sorts and Groups Methods ----------------
 
+#? Rows in a dataframe aren't usually in any specific order. We can re-sort our frame based on a column. For example, I can order the frame based on the duration column
+#? The dataframe prints out  in a new order, the row with the shortest durartion value will be first, and the other rows go in ascending order. 
+print(df.sort_values(by=["duration_ms"]))
+
+#? We can also group data. We saw earlier with value counts that I have 6 genres of playlist on my  dataframe. 
+#? Take a look at just the second half of the below code - ["duration_ms"].max() we know from above that this would find the longest song. 
+
+#? If I group my data by the playlist genre, my data gets split into 6 groups - one for each different genre, and finds out the longest song from each of those six categories!
+print(df.groupby('playlist_genre')["duration_ms"].max())
+
+#*  -------------  Queries ----------------
+
+#? We can use the .query() method to find specific results from our dataframe. 
+#? We hand a string over to the query method. That string is a comparison - here we are referencing the column track artist, but inside the rows of that column we are looking for the string 'Ricky Martin'
 print(df.query("track_artist=='Ricky Martin'"))
 
+#? I can reduce my dataframe down to results which are longer than the average length of a song.
 mean_val = df["duration_ms"].mean()
 
+#? Running
+#! print(df.query("duration_ms > mean_val"))
+#? would give me an error. The query is not looking into the surround scope to find our what mean_val is, it is only looking at the dataframe. 
+
+#? To instruct the frame to reference the outer scope (global scope) I can prefix my variable name with an @ symbol!
 print(df.query("duration_ms > @mean_val"))
